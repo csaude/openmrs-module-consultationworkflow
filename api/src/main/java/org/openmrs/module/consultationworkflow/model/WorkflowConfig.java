dc6dfb055a43c18d7dc146b58653710e3fcd8ff0
Eudson Bambo <eudson.bambo@gmail.com>
2025-03-13 12:57:50 +0200

    (feat) Changed FormResource to resourceValueReference

--- api/src/main/java/org/openmrs/module/consultationworkflow/model/WorkflowConfig.java
+++ api/src/main/java/org/openmrs/module/consultationworkflow/model/WorkflowConfig.java
@@ -4,7 +4,6 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.openmrs.BaseChangeableOpenmrsData;
-import org.openmrs.FormResource;
 
 import javax.persistence.*;
 import java.util.Set;
@@ -15,35 +14,30 @@ import java.util.Set;
 @Entity
 @Table(name = "consultationworkflow_workflow_config")
 public class WorkflowConfig extends BaseChangeableOpenmrsData {
-	
-	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	private Integer workflowConfigId;
-	
-	@Column(nullable = false)
-	private String name;
-	
-	@Column(nullable = false, length = 1000)
-	private String description;
-	
-	private Boolean published = false;
-	
-	@Column(nullable = false)
-	private String version;
-	
-	private FormResource formResource;
-	
-	@OneToMany(mappedBy = "workflowConfig", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
-	Set<EligibilityCriteria> criteriaSet;
-	
-	@Override
-	public Integer getId() {
-		return getWorkflowConfigId();
-	}
-	
-	@Override
-	public void setId(Integer id) {
-		setWorkflowConfigId(id);
-	}
-	
+
+    @OneToMany(mappedBy = "workflowConfig", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    Set<EligibilityCriteria> criteriaSet;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer workflowConfigId;
+    @Column(nullable = false)
+    private String name;
+    @Column(nullable = false, length = 1000)
+    private String description;
+    private Boolean published = false;
+    @Column(nullable = false)
+    private String version;
+    @Column(nullable = false)
+    private String resourceValueReference;
+
+    @Override
+    public Integer getId() {
+        return getWorkflowConfigId();
+    }
+
+    @Override
+    public void setId(Integer id) {
+        setWorkflowConfigId(id);
+    }
+
 }
